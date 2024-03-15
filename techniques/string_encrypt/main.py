1dbd75bf6d06f6f1e1071de468a91c9d33e62e24
Fun_Dan3 <dfr34560@gmail.com>
2024-03-15 03:25:43 +0300

    Fixed string_content persisting

--- techniques/string_encrypt/main.py
+++ techniques/string_encrypt/main.py
@@ -9,10 +9,10 @@ def encrypt_function(data, password):
 decrypt_function = f"''.join([chr(int(i)-int(j)) for i, j in zip(%s.split(';'), %s.split(';'))])"
 
 def obfuscate(code):
+	code = scan_for_string(code, "'")
+	code = scan_for_string(code, '"')
 	code = scan_for_string(code, "'''")
 	code = scan_for_string(code, '"""')
-	code = scan_for_string(code, '"')
-	code = scan_for_string(code, "'")
 	return code
 
 def scan_for_string(code, quote): # really bad string parser tbh
@@ -23,7 +23,10 @@ def scan_for_string(code, quote): # really bad string parser tbh
 	for i in range(len(code)):
 		if len(quote) == 3:
 			quote_check = code[i:i+len(quote)] == quote
-			in_tripple_quote = code[i] == quote[0] and (code[i-1] == quote[0] or code[i+1] == quote[0])
+			in_tripple_quote = code[i] == quote[0] and (
+				(code[i-1] == quote[0] and code[i-2] == quote[0]) or
+				(code[i+1] == quote[0] and code[i+2] == quote[0]) or
+				(code[i-1] == quote[0] and code[i+1] == quote[0]))
 			if not started and not in_tripple_quote:
 				return_code += code[i]
 			if started and not in_tripple_quote:
@@ -34,6 +37,7 @@ def scan_for_string(code, quote): # really bad string parser tbh
 						return_code += on_string(string_content, quote)
 					else:
 						return_code += quote + string_content + quote
+					string_content = ""
 				else:
 					special_string = code[i-1] in ["f", "b"] # f-string or b-string
 				started = not started
@@ -50,6 +54,7 @@ def scan_for_string(code, quote): # really bad string parser tbh
 						return_code += on_string(string_content, quote)
 					else:
 						return_code += quote + string_content + quote
+					string_content = ""
 				else:
 					special_string = code[i-1] in ["f", "b"] # f-string or b-string
 				started = not started
@@ -57,4 +62,4 @@ def scan_for_string(code, quote): # really bad string parser tbh
 
 def on_string(string_content, quote):
 	password = ";".join([str(random.randint(-99, 99)) for _ in range(len(string_content))])
-	return decrypt_function % ((quote + encrypt_function(string_content, password) + quote), '"' + password + '"')
+	return decrypt_function % (("'" + encrypt_function(string_content, password) + "'"), "'" + password + "'")

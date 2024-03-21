8bc9d9dda0dac3c09a4fd6189e0cbedd80744adb
Fun_Dan3 <dfr34560@gmail.com>
2024-03-21 21:33:51 +0300

    Reworked structure to make it pip library

--- main.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-import obfuscator
-
-with open("test_source.py", "r") as f:
-	code = f.read()
-code = obfuscator.obfuscate(code)
-with open("test_result.py", "w") as f:
-	f.write("#!/usr/bin/python3\n")
-	f.write(code)
--- src/yapo/obfuscator.py
+++ src/yapo/obfuscator.py
@@ -1,14 +1,9 @@
-import base64
 import techniques
 
 def obfuscate(code):
-	print("Decommenting...")
 	code = techniques.decomment(code)
-	print("Encrypting strings for main code...")
 	code = techniques.string_encrypt(code)
-	print("Base64 encoding main code...")
 	code = techniques.base64(code, decoder_function = "b64d(%s)")
-	print("Encrypting strings for encoded code...")
 	code = """b64 = __import__('base64')
 b64d = lambda c: b64.b64decode(c).decode('utf-8')
 """ + code
